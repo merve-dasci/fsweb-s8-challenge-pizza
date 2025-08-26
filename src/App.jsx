780d13997525f0baed768d3ba253610e5e07e904
merve-dasci <mervedscc97@gmail.com>
2025-08-26 11:13:04 +0300

    proje set edildi

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+     
       <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
+        Sprint 8 Pizza Challenge
       </p>
     </>
   )
